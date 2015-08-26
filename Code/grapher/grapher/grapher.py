7cfdabb302ac457e10818f11538d279af1d728ee
hbentz <hbentz@ualberta.ca>
2015-08-26 01:32:57 -0600

    Condenser Improvement + Added Graph Import
    Performance update for wikipedia xml condenser.  No longer rewrites the
    variables when checking for extra links in the line. Instead it starts
    looking later in the variable.
    
    Created code that can import the condensed wikipedia stuff into the
    snap.py graphs. Uses a 'for elements in graph' search on each to term to
    determine if  each term is unique before adding it to the graph. I
    suspect there is a more efficient way to do this. Run time on creating
    this graph looks like it is in the hundreds of hours. Currently I am
    running it. Let me know if you create something faster.

---
+++ Code/grapher/grapher/grapher.py
@@ -0,0 +1,164 @@
+﻿#For network analysis and pickle
+import pickle
+import snap
+
+#Specify the input file
+basic = open('basic.txt','r')
+
+#Export file for the map
+map = open('map.pckl','w')
+hashes = open('hashes.pckl','w')
+
+#create the graph
+G2 = snap.TNGraph.New()
+
+#index counter for number of nodes
+i = 0
+
+#is this a unique entry
+q = True
+
+#tokens to determine how to clasify the links
+tokOpen = '[['
+tokCol = ':'
+tokBar = "|"
+tokEnd = ']]'
+
+#hash table for checking link duplication
+h = snap.TIntStrH()
+
+#for every line
+for line in basic:
+
+    #find if the line contains links or titles
+    obsOpen = line.find(tokOpen)
+
+    #if it is a title
+    if obsOpen<0:
+        
+        #reset the unique entry
+        q = True
+
+        #reset the index counter
+        n = 0
+        
+        #for every index in the hashtable
+        for n in range(0,h.Len()):
+
+            #if an index exists that matches
+            if h[n] == str(line):
+
+                #this is not a unique entry
+                q = False         
+                       
+                #record the title index anyway
+                titleIndex = n
+
+                #stop searching
+                break
+
+        #if it is a unique entry
+        if q == True:
+
+            #add the unique entry to the hashtable
+            h[i] = str(line)
+
+            #and to the graph
+            G2.AddNode(i)
+
+            #record the title index
+            titleIndex = i
+
+            #increment the number of nodes
+            i = i+1
+            print(str(i))
+
+    #empty the text holder 
+
+    #while there is a still an undiscovered [[ in the line                                                                                                                            
+    while obsOpen>0:
+
+        #find a colon in the current link         
+        obsCol = line.find(tokCol,obsOpen+2)
+        
+        #find a pipe | in the current link
+        obsBar = line.find(tokBar,obsOpen+2)
+        
+        #if there is a colon
+        if obsCol>0:
+            
+            #disregard this link and start finding in the next one
+            obsOpen = line.find(tokOpen, obsOpen+2)
+            
+            #and jump to the next iteration (I think we can remove the previous 'if link needs to be writte n' if we put it after this break)
+            break
+
+        #if there is a pipe |
+        elif obsBar>0:
+            
+            #take stuff up to the pipe |
+            text = line[obsOpen+2:obsBar]
+            
+            #and start finding the next link
+            obsOpen = line.find(tokOpen, obsOpen+2)
+        
+        #otherwise
+        else:
+        
+            #find the end of the link
+            obsEnd = line.find(tokEnd,obsOpen)
+            
+            #take just the link text
+            text = line[obsOpen+2:obsEnd]
+            
+            #and find the next link
+            obsOpen = line.find(tokOpen, obsOpen+2)
+
+        #reset the unique entry indentifier
+        q = True
+
+        #reset the index counter
+        n = 0
+
+        #for every entry in the graph
+        for n in range(0,h.Len()):
+
+            #if this is the duplicate entry
+            if h[n] == str(text):
+
+                #note it
+                q = False
+
+                #record the link to index
+                currindex = n
+
+                #stop searching
+                break
+            
+        #if this a unique entry    
+        if q == True:
+
+            #put it in the hashtable
+            h[i] = str(text)
+                
+            #add it to the graph
+            G2.AddNode(i)
+
+            #record the link to index
+            currIndex = i
+
+            #and increment the index
+            i = i+1
+            print(str(i))
+        #assign the link
+        G2.AddEdge(titleIndex,currIndex)
+        print(str(currIndex))             
+              
+#dump the data
+pickle.dump(G2,map)
+pickle.dump(h,hashes)
+
+#close the files
+map.close
+h.close
+basic.close        
--- Code/wikigraph/wikigraph/wikigraph.py
+++ Code/wikigraph/wikigraph/wikigraph.py
@@ -8,28 +8,27 @@ tok2 = ']]'
 #Specify the tokens that indicate a new page
 tok3 = '<title>'
 tok4 = '</title>'
+i = 0;
 
 #Specify the output file
 basic = open('basic.txt','w')
 
 #specify the wikixml file
-with open("wiki.xml") as infile:
+with open("wiki.xml",'r') as infile:
 
     #for each line in the wiki file
     for line in infile:
-
         #try to find the 'new entry' token
         tok3p = line.find(tok3)
 
         #if this line is a new page
         if tok3p > 0:
-
             #find the end of the title
             tok4p = line.find(tok4)
-
+            i = i+1
             #write that line to the output file
-            basic.write(line[tok3p:tok4p+8:1]+'\r\n')
-
+            basic.write('\r\n'+line[tok3p+7:tok4p:1]+'\r\n')
+            print(str(i)+'  '+line[tok3p+7:tok4p:1])
         else:
             #otherwise try and find a link
             tok1p = line.find(tok1)
@@ -41,10 +40,10 @@ with open("wiki.xml") as infile:
             while tok1p>0:
 
                 #write that link to the output file
-                basic.write('     '+line[tok1p:tok2p+2:1]+'\r\n')
+                basic.write(' '+line[tok1p:tok2p+2:1])
                 
                 #find the next link and the end of that link
-                tok1p = line.find(tok1, tok2p)
-                tok2p = line.find(tok2, tok2p) 
+                tok1p = line.find(tok1, tok1p+2)
+                tok2p = line.find(tok2, tok2p+2)
 #close the file
 basic.close        
