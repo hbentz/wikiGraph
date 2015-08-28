55ba85d1e903be80c7246f5bf99446f6976996f7
hbentz <hbentz@ualberta.ca>
2015-08-27 22:20:20 -0600

    It Lives!!!
    So the trimmed xml file we made was all sorts of fucked, I updated the
    code to work with it properly a number of issues have been fixed:
    
    1) Article titles were imported with \r\n at their end, removed this and
    the names now match link names.
    
    2) If there was a : or | anywhere on the page the link text to be
    assigned was the start of the current link all the way up to the : or |.
    This has been fixed, probably not in the most efficient way.
    
    3) Added automatic dead end deletion.  If a new to the hash table
    article has no children it's value in the hash table is replaced with
    the next title. There are a number of wiki internal articles or articles
    that only link to wiki talk that this is effected by this change. This
    does require at minimum one variable reset and one if check per cycle.
    This only catches articles that have just been added to the hash table
    so this may not be worth it as we can pull the dead end articles out in
    post.
    
    Updated code comments come Monday after this is done running.

--- Code/grapher/grapher/grapher.py
+++ Code/grapher/grapher/grapher.py
@@ -5,15 +5,13 @@ import snap
 #Specify the input file
 basic = open('basic.txt','r')
 
-#Export file for the map
-map = open('map.pckl','w')
-hashes = open('hashes.pckl','w')
-
 #create the graph
 G2 = snap.TNGraph.New()
 
 #index counter for number of nodes
 i = 0
+an = 0
+skipNextxNodeCreations = 0
 
 #is this a unique entry
 q = True
@@ -30,12 +28,16 @@ h = snap.TIntStrH()
 #for every line
 for line in basic:
 
+    incr = False
+
     #find if the line contains links or titles
     obsOpen = line.find(tokOpen)
 
     #if it is a title
     if obsOpen<0:
-        
+        line = line[:-2]
+        print('Checking if '+line+' exists as a node (article number '+str(an)+')')
+
         #reset the unique entry
         q = True
 
@@ -46,13 +48,14 @@ for line in basic:
         for n in range(0,h.Len()):
 
             #if an index exists that matches
-            if h[n] == str(line):
+            if h[n] == line:
 
                 #this is not a unique entry
                 q = False         
                        
                 #record the title index anyway
                 titleIndex = n
+                print(line+' already exists at node '+str(n))
 
                 #stop searching
                 break
@@ -61,58 +64,68 @@ for line in basic:
         if q == True:
 
             #add the unique entry to the hashtable
-            h[i] = str(line)
+            h[i] = line
 
             #and to the graph
-            G2.AddNode(i)
-
+            if skipNextxNodeCreations>0:
+                skipNextxNodeCreations=skipNextxNodeCreations-1
+                print('Unique node creation for article number '+str(an)+' was skipped because node '+str(i)+' already exists')
+            else:
+                G2.AddNode(i)
             #record the title index
             titleIndex = i
 
             #increment the number of nodes
+            print(line+' has been assigned to node '+ str(i))
             i = i+1
-            print(str(i))
-
-    #empty the text holder 
+            
+        
+        an = an+1
+        #skip the rest of the loop
+        continue
+    
+    text = ''
 
     #while there is a still an undiscovered [[ in the line                                                                                                                            
     while obsOpen>0:
 
+        #find the end of the link
+        obsEnd = line.find(tokEnd,obsOpen+2)
+        
+        clicky = line[obsOpen+2:obsEnd]
+
         #find a colon in the current link         
-        obsCol = line.find(tokCol,obsOpen+2)
+        obsCol = clicky.find(tokCol)
         
         #find a pipe | in the current link
-        obsBar = line.find(tokBar,obsOpen+2)
+        obsBar = clicky.find(tokBar)
         
         #if there is a colon
-        if obsCol>0:
+        if obsCol>0 & (obsCol<obsBar | obsBar<0):
             
             #disregard this link and start finding in the next one
             obsOpen = line.find(tokOpen, obsOpen+2)
             
-            #and jump to the next iteration (I think we can remove the previous 'if link needs to be writte n' if we put it after this break)
+            #and jump to the next iteration (I think we can remove the previous 'if link needs to be written' if we put it after this break)
             break
 
         #if there is a pipe |
         elif obsBar>0:
             
             #take stuff up to the pipe |
-            text = line[obsOpen+2:obsBar]
+            text = clicky[0:obsBar]
             
             #and start finding the next link
             obsOpen = line.find(tokOpen, obsOpen+2)
         
         #otherwise
         else:
-        
-            #find the end of the link
-            obsEnd = line.find(tokEnd,obsOpen)
-            
+       
             #take just the link text
             text = line[obsOpen+2:obsEnd]
-            
+
             #and find the next link
-            obsOpen = line.find(tokOpen, obsOpen+2)
+            obsOpen = line.find(tokOpen, obsOpen+2) 
 
         #reset the unique entry indentifier
         q = True
@@ -124,14 +137,14 @@ for line in basic:
         for n in range(0,h.Len()):
 
             #if this is the duplicate entry
-            if h[n] == str(text):
+            if h[n] == text:
 
                 #note it
                 q = False
 
                 #record the link to index
                 currIndex = n
-
+                print('Found '+text+' at index '+str(n)+' assigning appropriate links...')
                 #stop searching
                 break
             
@@ -139,26 +152,37 @@ for line in basic:
         if q == True:
 
             #put it in the hashtable
-            h[i] = str(text)
+            h[i] = text
                 
             #add it to the graph
-            G2.AddNode(i)
-
+            if skipNextxNodeCreations>0:
+                skipNextxNodeCreations=skipNextxNodeCreations-1
+                print('Unique node creation for target '+text+' was skipped because node '+str(i)+' already exists')
+            else:
+                G2.AddNode(i)
             #record the link to index
             currIndex = i
 
             #and increment the index
+            print(text+' was assigned as a unique entry at index '+str(i))
             i = i+1
-            print(str(i))
         #assign the link
         G2.AddEdge(titleIndex,currIndex)
-        print(str(currIndex))             
+        print('An edge was created between nodes '+str(titleIndex)+' and '+str(currIndex))
+                 
+    if text == '':
+        print('No edges were created for article number '+str(an-1))
+        i = i-1
+        skipNextxNodeCreations=skipNextxNodeCreations+1   
+        print('The hash table index has been decremented to componsate, the next '+str(skipNextxNodeCreations)+' Node creations will be sklipped')                    
               
-#dump the data
-pickle.dump(G2,map)
+#Export file for the map
+map = TFOut('map.graph')
+G2.Save(map)
+
+hashes = open('hashes.pckl','w')
 pickle.dump(h,hashes)
 
 #close the files
-map.close
-h.close
-basic.close        
+basic.close
+hashes.close
